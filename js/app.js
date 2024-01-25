c1057f8e629ba11969f78287c1fa167b60c93b22
Sofiia Kosachevych <sofiakosachvych@gmail.com>
2024-01-25 12:11:57 -0600

    creating a new repo for js-arrays

---
+++ js/app.js
@@ -0,0 +1,35 @@
+let movies = ['big Little Lies', 'sexLife', 'elite'];
+
+// const lastItem = movies[movies.length - 1]
+// console.log(lastItem);
+
+// movies.push("you", "my life with the Walter Boys")
+// console.log(movies)
+
+// movies.pop()
+// console.log(movies);
+
+// movies.shift()
+// movies.unshift("first")
+// console.log(movies);
+
+// // movies.splice(start, deleteCount, item1, item2,...)
+
+// //remove the second item in the array and add 2 movies in the array using the splice
+// movies.splice(1, 1, 'riverdale', 'the Society')
+// console.log(movies);
+
+
+// movies.forEach(function(movie) {
+//   //I want to uppercase the first letter in movie
+//   // this method creates a word (the first letter is capitalised and the next ones are taken from an original name of the movie (except for the first letter))
+// const capitalised = movie[0].toUpperCase() + movie.substring(1)
+
+//   console.log(`I want to see ${capitalised}`)
+// })
+ 
+// spread operator
+// let arr1 = [1,2,3]
+// let arr2 = [...arr1, 4, 5, 6,]
+// console.log(arr2);
+
